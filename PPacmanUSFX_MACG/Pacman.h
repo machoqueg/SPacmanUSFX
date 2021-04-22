0eada6b344cb5ec22851658599e709d8026aa769
machoqueg <mickychog31@gmail.com>
2021-04-21 22:10:27 -0400

    Se incluye el objeto textura

--- PPacmanUSFX_MACG/Pacman.h
+++ PPacmanUSFX_MACG/Pacman.h
@@ -7,18 +7,25 @@ private:
 	int color;
 	float velocidadX;
 	float velocidadY;
-public:
+	SDL_Point velocidad;
 
+public:
 	void move(float _velocidadX, float _velocidadY) {};
 
 	SDL_Point getPosicion() { return posicion; }
+	int getPosicionX() { return posicion.x; }
+	int getPosicionY() { return posicion.y; }
+
 	void setPosicion(SDL_Point _posicion) { posicion = _posicion; }
-	
-	
+	void setPosicionX(int _x) { posicion.x = _x; }
+	void setPosicionY(int _y) { posicion.y = _y; }
 
-	float getVelocidadX() { return velocidadX; }
-	void setVelocidadX(float _velocidadX) { velocidadX = _velocidadX; }
-	float getVelocidadY() { return velocidadY; }
-	void setVelocidadY(float _velocidadY) { velocidadY = _velocidadY; }
+	SDL_Point getVelocidad() { return velocidad; }
+	int getVelocidadX() { return velocidad.x; }
+	int getVelocidadY() { return velocidad.y; }
+
+	void setVelocidad(SDL_Point _velocidad) { velocidad = _velocidad; }
+	void setVelocidadX(int _x) { velocidad.x = _x; }
+	void setVelocidadY(int _y) { velocidad.y = _y; }
+
 };
-
